dc5ae3ebaac598fb1d220b4825e5ed100711cc25
C-handan <152592546+C-handan@users.noreply.github.com>
2025-01-09 23:59:49 +0530

    competition completed, sponsors done

--- src/app/competitions/page.js
+++ src/app/competitions/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./components/searchbar";
 import CloudSection from "./components/Cloudsection";
+import Cards from "./components/competitionCard";
 export default function Page() {
   return (
     <div>
