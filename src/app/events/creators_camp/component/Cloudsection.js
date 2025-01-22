277563e8da2399b752a5216e2c760d69855e766a
maidevalhoon <devalsinghal355@gmail.com>
2025-01-22 13:56:57 +0530

    cloud part to be sorted

--- src/app/events/creators_camp/component/Cloudsection.js
+++ src/app/events/creators_camp/component/Cloudsection.js
@@ -1,84 +1,40 @@
 import Image from "next/image";
 import SlotCounter from "react-slot-counter";
-import styles from "./components.module.css";
+import styles from "./cloudsection.module.css";
+import Marquee from "react-fast-marquee";
+import { Color } from 'three';
 const CloudSection = () => {
   return (
-    <section className={styles.bgset}>
-      <section className="clouds">
-        <div>
-          <Image
-            src="/cloud1u.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="fu cloud "
-          />
-          <Image
-            src="/cloud1b.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="fb cloud "
-          />
-        </div>
-        <div>
-          <Image
-            src="/cloud2u.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="su cloud "
-          />
-          <Image
-            src="/cloud2d.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="sd cloud "
-          />
-        </div>
-        <div>
-          <Image
-            src="/cloud3.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="tu  cloud "
-          />
-        </div>
-        <div>
-          <Image
-            src="/cloudlu.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="lu cloud "
-          />
-          <Image
-            src="/cloudlb.png"
-            alt="cloud"
-            width={100}
-            height={100}
-            className="lb cloud "
-          />
-        </div>
-      </section>
-      <div className={styles.header}>
-        <p className={styles.title}>Creator's Camp</p>
-        <p className={styles.oneline}>
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ab?
-          Fuga, libero reprehenderit quidem nobis"
-        </p>
-      </div>
-      <div className={styles.content}>
+    <div className={styles.galleryclouds}>
+    <div className={styles.galleryclouds1}>
+        <Marquee speed={10} >
+            <Image src="./cloudLeft.svg" width="1000" height="1000" />
+            <Image src="./cloudLeft.svg" width="1000" height="1000" />
+            <Image src="./cloudLeft.svg" width="1000" height="1000" />
+        </Marquee>
+    </div>
+    <div className={styles.galleryclouds2}>
+        <Marquee direction="right" speed={30} >
+            <Image src="./cloudRight.svg" width="1000" height="1000" />
+            <Image src="./cloudRight.svg" width="1000" height="1000" />
+            <Image src="./cloudRight.svg" width="1000" height="1000" />
+        </Marquee>
+    </div>
+    <div className={styles.galleryHeader}>
+    <div className={styles.heading}>Gallery</div>
+    <div className={styles.topsubheading}>“Unite for change, ignite hope, and
+        let people's voices shape our journey.“</div>
+       
+        
+        <div className={styles.contents}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
           tristique purus eu libero egestas, sed bibendum felis tincidunt. Proin
           ultricies viverra mollis. Nullam vel quam tortor. Duis egestas libero
           velit, vel ullamcorper ipsum condimentum ac. Suspendisse ac porttitor
         </p>
-      </div>
-    </section>
+      </div></div>
+</div>
   );
 };
 
---
+++ src/app/events/pronites/component/comingsoon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./comingsoon.module.css";
+export default function ComingSoon() {
+  return (
+    <div className={styles.main}>
+        <div className={styles.center}>COMING SOON !!!</div>
+    </div>
+  )
+}
