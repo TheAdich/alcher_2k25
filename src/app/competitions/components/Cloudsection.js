31326f5d9186517faf243bb6eacae967c70e1bdf
C-handan <152592546+C-handan@users.noreply.github.com>
2025-01-24 02:44:15 +0530

    sponsors photos added

--- src/app/competitions/components/Cloudsection.js
+++ src/app/competitions/components/Cloudsection.js
@@ -2,39 +2,35 @@ import Image from "next/image";
 import SlotCounter from "react-slot-counter";
 import styles from "./components.module.css";
 import Marquee from "react-fast-marquee";
-import { Color } from 'three';
+import { Color } from "three";
 
-const CloudSection = ({event}) => {
+const CloudSection = ({ event }) => {
   const { name, topsubheading, contents } = event[0];
   return (
-
     <div className={styles.galleryclouds}>
-    <div className={styles.galleryclouds1}>
-        <Marquee speed={10} >
-            <Image src="/cloudLeft.svg" width="1000" height="1000" />
-            <Image src="/cloudLeft.svg" width="1000" height="1000" />
-            <Image src="/cloudLeft.svg" width="1000" height="1000" />
+      <div className={styles.galleryclouds1}>
+        <Marquee speed={10}>
+          <Image src="/cloudLeft.svg" width="1000" height="1000" />
+          <Image src="/cloudLeft.svg" width="1000" height="1000" />
+          <Image src="/cloudLeft.svg" width="1000" height="1000" />
         </Marquee>
-    </div>
-    <div className={styles.galleryclouds2}>
-        <Marquee direction="right" speed={30} >
-            <Image src="/cloudRight.svg" width="1000" height="1000" />
-            <Image src="/cloudRight.svg" width="1000" height="1000" />
-            <Image src="/cloudRight.svg" width="1000" height="1000" />
+      </div>
+      <div className={styles.galleryclouds2}>
+        <Marquee direction="right" speed={30}>
+          <Image src="/cloudRight.svg" width="1000" height="1000" />
+          <Image src="/cloudRight.svg" width="1000" height="1000" />
+          <Image src="/cloudRight.svg" width="1000" height="1000" />
         </Marquee>
-    </div>
-    <div className={styles.galleryHeader}>
-    <div className={styles.heading}>{name}</div>
-    <div className={styles.topsubheading}>{topsubheading}</div>
-       
-        
-        <div className={styles.contents}>
-        <p>
-        {contents}
+      </div>
+      <div className={styles.galleryHeader}>
+        <div className={styles.heading}>{name}</div>
+        <div className={styles.topsubheading}>{topsubheading}</div>
 
-        </p>
-      </div></div>
-</div>
+        <div className={styles.contents}>
+          <p>{contents}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
