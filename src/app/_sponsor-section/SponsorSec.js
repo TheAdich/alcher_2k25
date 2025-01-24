c6e5969068ac36250c7805dd8856c4d2c5af7d6b
C-handan <152592546+C-handan@users.noreply.github.com>
2025-01-24 17:26:32 +0530

    comp page registration link added in register img

--- src/app/_sponsor-section/SponsorSec.js
+++ src/app/_sponsor-section/SponsorSec.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import styles from "./sponsor.module.css";
-import img1 from "./_components/img1.png";
+import img1 from "./_components/gplus.png";
 import img2 from "./_components/img2.png";
 import img3 from "./_components/img3.png";
 import img4 from "./_components/img4.png";
@@ -30,60 +30,74 @@ const sponsors = [
   { name: "Gplus", logo: img1, alt: "Gplus logo" },
   { name: "MCJ", logo: img15, alt: "MCJ" },
   { name: "Nerim", logo: img6, alt: "Nerim" },
-  
 ];
 
 const SponsorSection = () => {
-
-  const [gridDimensions, setGridDimensions] = useState({ width: 300, height: 50,gridsize:16.5 });
+  const [gridDimensions, setGridDimensions] = useState({
+    width: 300,
+    height: 50,
+    gridsize: 16.5,
+  });
 
   useEffect(() => {
-      const updateDimensions = () => {
-        if (typeof window !== "undefined") {
-          if (window.innerWidth > 1024) {
-            setGridDimensions({ width: 3000, height: 530, gridsize: 15 }); 
-          } else if (window.innerWidth > 991) {
-            setGridDimensions({ width: 2000, height: 95, gridsize: 14 }); 
-          } else if (window.innerWidth > 768) {
-            setGridDimensions({ width: 2000, height: 85, gridsize: 11 }); 
-          } else {
-            setGridDimensions({ width: 0, height: 0, gridsize: 0 });
-          }
+    const updateDimensions = () => {
+      if (typeof window !== "undefined") {
+        if (window.innerWidth > 1024) {
+          setGridDimensions({ width: 3000, height: 530, gridsize: 15 });
+        } else if (window.innerWidth > 991) {
+          setGridDimensions({ width: 2000, height: 95, gridsize: 14 });
+        } else if (window.innerWidth > 768) {
+          setGridDimensions({ width: 2000, height: 85, gridsize: 11 });
+        } else {
+          setGridDimensions({ width: 0, height: 0, gridsize: 0 });
         }
-      };
-  
-      updateDimensions(); // Initial call
-      window.addEventListener("resize", updateDimensions); // Add listener
-  
-      return () => {
-        window.removeEventListener("resize", updateDimensions); // Cleanup
-      };
-    }, []);
+      }
+    };
 
+    updateDimensions(); // Initial call
+    window.addEventListener("resize", updateDimensions); // Add listener
 
+    return () => {
+      window.removeEventListener("resize", updateDimensions); // Cleanup
+    };
+  }, []);
 
   return (
     <div className={styles.sponsorsection}>
       <div className={styles.dashContainerup}>
-      <GridSketch width={gridDimensions.width} height={gridDimensions.height} gridsize={gridDimensions.gridsize} />
+        <GridSketch
+          width={gridDimensions.width}
+          height={gridDimensions.height}
+          gridsize={gridDimensions.gridsize}
+        />
         <Image src={dash1} alt="Top dash" className={styles.dash} />
       </div>
       <h2 className={styles.sponsortitle}>Sponsors</h2>
-      <Image src={line2} className={styles.lines} alt="line2"/>
+      <Image src={line2} className={styles.lines} alt="line2" />
       <div className={styles.sponsorlogos}>
         {sponsors.map((sponsor, index) => (
-          
-          <div className={`${styles.sponsorlogo} ${index === 1 ? styles.SSlogo : ""}`}
-          key={index} >
-
-            <Image src={sponsor.logo} alt={sponsor.alt} className={styles.sponsorImg} />
+          <div
+            className={`${styles.sponsorlogo} ${
+              index === 1 ? styles.SSlogo : ""
+            }`}
+            key={index}
+          >
+            <Image
+              src={sponsor.logo}
+              alt={sponsor.alt}
+              className={styles.sponsorImg}
+            />
           </div>
         ))}
-        <Image/>
+        <Image />
       </div>
-       <Image src={line2} className={styles.lines} alt="ewdhskg"/>
+      <Image src={line2} className={styles.lines} alt="ewdhskg" />
       <div className={styles.dashContainer1}>
-      <GridSketch width={gridDimensions.width} height={gridDimensions.height} gridsize={gridDimensions.gridsize} />
+        <GridSketch
+          width={gridDimensions.width}
+          height={gridDimensions.height}
+          gridsize={gridDimensions.gridsize}
+        />
         <Image src={dash2} alt="Bottom dash" className={styles.dash} />
       </div>
     </div>
@@ -91,4 +105,3 @@ const SponsorSection = () => {
 };
 
 export default SponsorSection;
-
