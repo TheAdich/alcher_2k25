fc580e0323e6ed274a55a0542a45ee3f2951a241
Pavan Lokesh <pavanlokesh.developer@gmail.com>
2025-01-25 19:29:27 +0530

    Changed Oversized model and added texture

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="textureDiv">
         <Navbar />
         {children}
         <Footer />
+        </div>
       </body>
     </html>
   );
