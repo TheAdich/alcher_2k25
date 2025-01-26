840fc3cb6f0cce1c35fd08859e943ca98604c563
riderHimank <himankb500@gmail.com>
2025-01-26 12:52:20 +0530

    SEO keywords

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,7 +1,9 @@
 import dynamic from "next/dynamic";
 import Loading from "../loader";
 export const metadata = {
-  title: 'Events|Alcheringa',
+  title: 'Events | Alcheringa',
+  description: 'The stage is set, the night is calling, and the magic is about to unfold! 🌟 Join us at Crescendo Night for an unforgettable performance by Amit Trivedi ! 🎤',
+  keywords: 'Amit Trivedi, amit trivedi'
 }
 
 const Page = dynamic(() => import('./events'), {
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import dynamic from "next/dynamic";
 import Loading from "./loader.jsx";
 export const metadata = {
   title: "Alcheringa",
+  description:
+    "Alcheringa is the annual cultural extravaganza of IIT Guwahati, a vibrant celebration of youth, creativity, and diversity. As North East India's largest cultural festival, Alcheringa offers a unique platform for students to showcase their talents and explore a world of artistic expression.  Experience three days and four nights of electrifying performances, thrilling competitions, and unforgettable moments. From January 30th to February 2nd, 2025, Alcheringa invites you to unlock a world of artistic expression and cultural exploration.",
+  keywords:
+    "Alcheringa, IIT Guwahati , Guwahati , Amit Trivedi , Pronite , North East , Cultural , Fest",
 };
 
 const Page = dynamic(() => import("./home"), {
